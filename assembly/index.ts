db5740ded695a3306a3e7d9e65da2051dd196d78
Bryant Hagadorn <blhagadorn@gmail.com>
2020-04-24 00:23:37 -0400

    Added XSS Protection header

--- assembly/index.ts
+++ assembly/index.ts
@@ -25,9 +25,7 @@ class AddHeader extends Context {
   onResponseHeaders(a: u32): FilterHeadersStatusValues {
     const root_context = this.root_context;
     if (root_context.configuration == "") {
-      stream_context.headers.response.add("hello", "world!");
-    } else {
-      stream_context.headers.response.add("hello", root_context.configuration);
+      stream_context.headers.response.add('X-XSS-Protection', '1');
     }
     return FilterHeadersStatusValues.Continue;
   }
